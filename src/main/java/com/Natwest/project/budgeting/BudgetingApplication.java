ba0467bca90e86fce1821ba98dfaf46558938477
1192817 <rashishrivastavarr@gmail.com>
2023-09-25 12:32:26 +0530

    Initial commit

---
+++ src/main/java/com/Natwest/project/budgeting/BudgetingApplication.java
@@ -0,0 +1,13 @@
+package com.Natwest.project.budgeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Natwest/project/budgeting/BudgetingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Natwest.project.budgeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
